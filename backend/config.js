d4ad58ef8ef29f7cf196131d5546e877223b6b14
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-06-14 14:46:06 +0700

    Complete backend api and set up for frontend

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 3001;
+export const MONGODB_URL =
+  "mongodb+srv://akm:3Wct4qflppjsB8G7@books-store-mern.oymtlhp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+// Middleware for handling CORS Policy
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to Mern Stack Tuto.");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("App connected to database.");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
